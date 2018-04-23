1f90630aeded8859a54df08ad4d7fa3fe4ecbafb
J. Chitty <jesuschitty7@hotmail.com>
2018-04-23 16:14:58 -0300

    Create sbd_sender.py

---
+++ scripts/sbd_sender.py
@@ -0,0 +1,20 @@
+# PYTHON quick SBD sender
+from steem import Steem
+s = Steem(
+	nodes=["http://rpc.buildteam.io"],
+	keys=["PRIV POSTING", "PRIV ACTIVE"]
+)
+print ("welcome to quick SBD Sender")
+to = input("Please provide receiver account: ")
+amount1 = input("Please provide amount to send: ")
+amount = float(amount1)
+memo = input("Please provide a memo (optional) or press enter to continue: ")
+
+s.commit.transfer(
+    to, 
+    amount, 
+    "SBD", 
+    memo= memo, 
+    account="hcf27"
+)
+print (amount1 + " SBD has been sent to " + to)
