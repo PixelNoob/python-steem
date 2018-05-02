14d6f9c5d50384ebf23cb800798776f503c4f351
J. Chitty <jesuschitty7@hotmail.com>
2018-05-02 15:38:16 -0300

    Create chibi.py

---
+++ scripts/chibi.py
@@ -0,0 +1,28 @@
+from steem import Steem
+import urllib.request
+import json
+import random
+import json
+
+chibi_number = random.randint(1, 800)
+chibi = urllib.request.urlopen('https://chibigame.io/chibis.php?g&idj=' + str(chibi_number))
+result = chibi.read()
+result_json = json.loads(result)
+
+chibi_name = (result_json["name"])
+avatar = (result_json["imageUrl"])
+description = (result_json["description"])
+
+print ("fetching chibi name..." + chibi_name)
+print ("fetching avatar URL... " + avatar)
+print ("fetching description..." + description)
+print ("posting to Steemit.com")
+
+s = Steem(keys=["YOUR PRIV KEY"])
+s.commit.post(
+    "Daily Chibi: {}".format(chibi_name),
+    "**{}**".format(chibi_name) + "<br> {}".format(avatar) + "<br>**Description:** {}<br>".format(description) + "<br>*This post was posted by the [Daily Chibi Poster](https://github.com/PixelNoob/python-steem)* <br>",
+    "YOUR ACCOUNT",
+    tags=["game","chibifighter", "chibigame"]
+)
+print ("your Chibi has been posted succesfully")
