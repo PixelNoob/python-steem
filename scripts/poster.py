f2cf28d13249be4087ab639fcdc3c5b25c09b978
J. Chitty <jesuschitty7@hotmail.com>
2018-10-31 11:57:57 -0300

    Create poster.py
    Create a simple poster utility.

---
+++ scripts/poster.py
@@ -0,0 +1,15 @@
+# This bot will post content on the steem blockchain
+
+from steem import Steem
+import json
+
+print ("posting on the steem blockchain...")
+s = Steem(keys=["YOUR_PRIV_POSTING_KEY"])
+s.commit.post(
+    "Posting using Steem Python Lib", # title
+    "This is a test and should not be taken seriously", # body
+    "vinotinto", # account
+    tags=["spam", "test"] # tags
+)
+
+print ("Posted to the the Blockchain, see link: " + "https://steemit.com/@vinotinto")
