0f77da6edacd0d9863b3f428e555fe6079a40584
J. Chitty <jesuschitty7@hotmail.com>
2019-01-14 17:03:51 -0300

    Update memo-sender.sh

--- memo-sender/memo-sender.sh
+++ memo-sender/memo-sender.sh
@@ -4,7 +4,7 @@ OLDIFS=$IFS
 IFS=","
 while read account
     do
-    	UNLOCK=<YOUR STEEMPY PASSWORD> steempy transfer --account YOURACCOUNT $account 0.001 "YOUR MEMO HERE"
+    	UNLOCK=<YOUR STEEMPY PASSPHRASE> steempy transfer --account <YOURACCOUNT> $account 0.001 <"YOUR MEMO HERE">
       sleep 1
 done < $1
 IFS=$OLIFS
